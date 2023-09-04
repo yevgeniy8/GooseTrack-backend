a9cb49c2e01f8318200a704b118cfd5e8bccde31
Yevgeniy Utkin <116801272+yevgeniy8@users.noreply.github.com>
2023-09-04 21:39:23 +0300

    done hw3

---
+++ controllers/contact.js
@@ -0,0 +1,120 @@
+const Book = require('../models/contact');
+
+const schema = require('../schema/schema');
+
+const getAll = async (req, res, next) => {
+    try {
+        const contacts = await Book.find();
+        res.json(contacts);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const getById = async (req, res, next) => {
+    try {
+        const contact = await Book.findById(req.params.contactId);
+        if (!contact) {
+            return next();
+        }
+        res.json(contact);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const create = async (req, res, next) => {
+    try {
+        const { error } = schema.addSchema.validate(req.body);
+        if (error) {
+            return res.status(400).json({
+                message: `missing required ${error.message
+                    .split(' ')[0]
+                    .slice(1, -1)} field`,
+            });
+        }
+
+        const { name, email, phone } = req.body;
+
+        const contact = {
+            name,
+            email,
+            phone,
+        };
+        const doc = await Book.create(contact);
+        res.status(201).json(doc);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const remove = async (req, res, next) => {
+    try {
+        const contact = await Book.findByIdAndDelete(req.params.contactId);
+        if (!contact) {
+            next();
+            return;
+        }
+        res.json({ message: 'contact deleted' });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const update = async (req, res, next) => {
+    try {
+        const { error } = schema.addSchema.validate(req.body);
+        if (error) {
+            return res.status(400).json({
+                message: 'missing fields',
+            });
+        }
+
+        const contact = await Book.findByIdAndUpdate(
+            req.params.contactId,
+            req.body,
+            { new: true }
+        );
+
+        if (!contact) {
+            next();
+            return;
+        }
+        res.status(200).json(contact);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const updateStatusContact = async (req, res, next) => {
+    try {
+        const { contactId } = req.params;
+        const { error } = schema.updateFavoriteSchema.validate(req.body);
+        if (error) {
+            return res.status(400).json({
+                message: 'missing field favorite',
+            });
+        }
+        const contact = await Book.findByIdAndUpdate(contactId, req.body, {
+            new: true,
+        });
+
+        if (!contact) {
+            next();
+            return;
+        }
+
+        res.status(200).json(contact);
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    remove,
+    update,
+    updateStatusContact,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,90 +1,19 @@
 const express = require('express');
 
-const contactsMethods = require('../../models/contacts');
-
-const schema = require('../../schema/schema');
+const ContactController = require('../../controllers/contact');
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-    try {
-        const contacts = await contactsMethods.listContacts();
-        res.json(contacts);
-    } catch (error) {
-        next(error);
-    }
-});
+router.get('/', ContactController.getAll);
 
-router.get('/:contactId', async (req, res, next) => {
-    try {
-        const contact = await contactsMethods.getContactById(
-            req.params.contactId
-        );
-        if (!contact) {
-            return next();
-        }
-        res.json(contact);
-    } catch (error) {
-        next(error);
-    }
-});
+router.get('/:contactId', ContactController.getById);
 
-router.post('/', async (req, res, next) => {
-    try {
-        const { error } = schema.validate(req.body);
-        if (error) {
-            return res.status(400).json({
-                message: `missing required ${error.message
-                    .split(' ')[0]
-                    .slice(1, -1)} field`,
-            });
-        }
+router.post('/', ContactController.create);
 
-        const { name, email, phone } = req.body;
-        const contact = await contactsMethods.addContact(name, email, phone);
-        res.status(201).json(contact);
-    } catch (error) {
-        next(error);
-    }
-});
+router.delete('/:contactId', ContactController.remove);
 
-router.delete('/:contactId', async (req, res, next) => {
-    try {
-        const contact = await contactsMethods.removeContact(
-            req.params.contactId
-        );
-        if (!contact) {
-            next();
-            return;
-        }
-        res.json({ message: 'contact deleted' });
-    } catch (error) {
-        next(error);
-    }
-});
+router.put('/:contactId', ContactController.update);
 
-router.put('/:contactId', async (req, res, next) => {
-    try {
-        const { error } = schema.validate(req.body);
-        if (error) {
-            return res.status(400).json({
-                message: 'missing fields',
-            });
-        }
-
-        const contact = await contactsMethods.updateContact(
-            req.params.contactId,
-            req.body
-        );
-
-        if (!contact) {
-            next();
-            return;
-        }
-        res.json(contact);
-    } catch (error) {
-        next(error);
-    }
-});
+router.patch('/:contactId/favorite', ContactController.updateStatusContact);
 
 module.exports = router;
