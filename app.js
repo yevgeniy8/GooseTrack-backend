4fead9dbcf4efc67623cae8083c9596d705f1b33
Yevgeniy Utkin <116801272+yevgeniy8@users.noreply.github.com>
2023-09-27 15:57:46 +0300

    fix repo

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 
-const usersRouter = require('./routes/api/users');
-const contactsRouter = require('./routes/api/contacts');
-
 const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
@@ -13,9 +10,6 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
-
-app.use('/users', usersRouter);
-app.use('/api/contacts', contactsRouter);
 
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found' });
